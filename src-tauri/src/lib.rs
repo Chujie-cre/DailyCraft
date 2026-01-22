39a258c82ccd745168147afdcdaabe307dfad7bb
Chujie-cre <2425739349@qq.com>
2026-01-22 15:44:30 +0800

    注册Tauri插件和命令处理器 - 注册opener和dialog插件 - 注册所有Tauri命令到invoke_handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,29 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+pub mod commands;
+pub mod error;
+pub mod models;
+pub mod services;
+
+use commands::system::*;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            get_active_window,
+            record_app_focus,
+            get_data_dir,
+            set_data_dir,
+            init_today_storage,
+            get_today_event_count,
+            record_keyboard_event,
+            record_mouse_event,
+            record_idle_event,
+            get_today_events_grouped,
+            get_today_events,
+            get_icon_for_app
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
