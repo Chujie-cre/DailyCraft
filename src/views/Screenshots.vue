5df55a32a2641f8dec8d0c0f179ed3e64e070c77
Chujie-cre <2425739349@qq.com>
2026-01-25 03:14:16 +0800

    style(screenshots): 固定图片显示区域高度为 400px

--- src/views/Screenshots.vue
+++ src/views/Screenshots.vue
@@ -522,7 +522,9 @@ onMounted(() => {
 
 .image-container {
   flex: 1;
-  height: 100%;
+  height: 400px;
+  min-height: 400px;
+  max-height: 400px;
   display: flex;
   align-items: center;
   justify-content: center;
